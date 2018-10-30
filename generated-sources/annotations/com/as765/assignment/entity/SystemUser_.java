6093411a43977123a7f4b07bfac983c37a42da5d
ArsalanS83 <44607985+ArsalanS83@users.noreply.github.com>
2018-10-30 13:05:29 +0000

    Add files via upload

---
+++ generated-sources/annotations/com/as765/assignment/entity/SystemUser_.java
@@ -0,0 +1,15 @@
+package com.as765.assignment.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-18T01:51:07")
+@StaticMetamodel(SystemUser.class)
+public class SystemUser_ { 
+
+    public static volatile SingularAttribute<SystemUser, Long> id;
+    public static volatile SingularAttribute<SystemUser, String> username;
+    public static volatile SingularAttribute<SystemUser, String> userpassword;
+
+}
